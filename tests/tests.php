b557e21a7e7e42299ad59d586c63f0fb935d58f8
Cyril COLINET <cyrilcolinet.pro@gmail.com>
2018-01-22 13:47:53 +0100

    :snail: Update tests

--- tests/tests.php
+++ tests/tests.php
@@ -7,7 +7,11 @@ class Test extends PHPUnit_Framework_TestCase
 
     public function testExample()
     {
-        $result = Example\Example::go();
+		if (false) {
+			return true;
+		}
+
+		return false;
     }
 
 }
